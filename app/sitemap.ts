0db8fa934b46a150b330ebe63ab53beaf6586102
Movchan Vladyslav <movchan.vladyslav@ynov.com>
2026-06-24 12:56:07 +0200

    t

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,11 @@
 import type { MetadataRoute } from 'next'
 
+const siteUrl = process.env.NEXT_PUBLIC_URL!
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: 'https://....com',
+      url: siteUrl,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
